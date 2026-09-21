c055d808fa2e969a1b778a089e0043bd23e64017
ryanhenrqq <ryanh8116@gmail.com>
2026-09-21 12:03:26 -0300

    feature: added an automatic gps search on load, and added a gps search button - using geolocation browser features

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,8 +5,9 @@ import type { HeaderFunction } from '../../types/types'
 
 import homeIcon from '../../assets/home.png'
 import infoIcon from '../../assets/info.png'
-import logo from '/public/favicon.png'
 import searchIcon from '../../assets/search.png'
+import logo from '/public/favicon.png'
+import gpsIcon from '../../assets/gps.png'
 import closeIcon from '../../assets/close.png'
 import { Attributes } from '../attributes/attributes'
 
@@ -15,20 +16,23 @@ export function Header({onSearch, onBack, error}: HeaderFunction) {
   const [infosView, setInfosView] = useState(false)
   // AS LINHAS IGNORADAS ABAIXO FORAM APENAS PRA CONSEGUIR FAZER O COMMIT E ATUALIZAR A API NO VERCER (MUDANÇAS DO WEATHER.TS PRA ACEITAR LATITUDE E LONGITUDE)
   const [loading, setLoading] = useState(false)
+  const [lastLat, setLastLat] = useState(0)
+  const [lastLon, setLastLon] = useState(0)
 
-  const handleSearch = () => {
+  const handleSearch = (city: string) => {
     console.log("click - header")
     if (!city.trim()) return
     console.log(city)
     onSearch(city)      // descobrir como limpar o campo de pesquisa apos a funçao dar certo sem afetar aqui!!
   }
+
   const handleInfosView = () => {
     !infosView?setInfosView(true):setInfosView(false)
   }
 
   const handleGeolocationSearch = async(lat: number, lon: number) => {
     setLoading(true)
-    console.log(lat, lon)
+    let cityname = ''
     try {
         const res = await fetch(`https://rweather-alpha.vercel.app/api/weather?lat=${lat}&lon=${lon}`)
         if (!res.ok){
@@ -36,27 +40,37 @@ export function Header({onSearch, onBack, error}: HeaderFunction) {
             throw new Error(errData.error || 'essas coords não foram encontradas')
         }
         const data = await res.json()
-        console.log(data)
+        console.log(data.name)
+        cityname = data.name
     } catch (err: any) {
         console.error(err)
     } finally {
         setLoading(false)
     }
+
+    handleSearch(cityname)
+    }
+
+    const handleGeoCatch = () => {
+        console.log('click')
+        if (navigator.geolocation) { //fins de testes, objetivo de pesquisar sozinho ao entrar no site
+            console.log('run')
+            navigator.geolocation.getCurrentPosition((pos) => {
+                const lat = pos.coords.latitude
+                const lon = pos.coords.longitude
+                setLastLat(lat)
+                setLastLon(lon)
+                handleGeolocationSearch(lat, lon)
+            }, (error) => {
+                console.error(`Erro ao conseguir geoloc: ${error}`)
+            })
+        } else {
+            console.log('Geolocation não suportado!')
+        }
     }
 
     useEffect(() => {
-        if (navigator.geolocation) { //fins de testes, objetivo de pesquisar sozinho ao entrar no site
-            navigator.geolocation.getCurrentPosition((pos) => {
-            const lat = pos.coords.latitude
-            const lon = pos.coords.longitude
-            console.log(`Latitude: ${lat}, Longitude: ${lon}`)
-            handleGeolocationSearch(lat, lon)
-        }, (error) => {
-            console.error(`Erro ao conseguir geoloc: ${error}`)
-        })
-    } else {
-        console.log('Geolocation não suportado!')
-    }
+        handleGeoCatch()
     }, [])
 
     useEffect(() => {
@@ -81,15 +95,16 @@ export function Header({onSearch, onBack, error}: HeaderFunction) {
                     if (e.key==="Enter"){
                         console.log("DISPARADO PELO TECLADO") // para debug
                         e.preventDefault()
-                        handleSearch()
+                        handleSearch(city)
                     }
                 }}
-                />
-            <img src={searchIcon} alt="Pesquisar" onClick={handleSearch} />
+            />
+            <img src={searchIcon} alt="Pesquisar" onClick={() => handleSearch(city)} />
           </div>
           {error==''?<span></span>:<small style={{color:'red'}}>Cidade nao encontrada</small>}
         </div>
         <div className="right-side-header flex-hor-align">
+            <img src={gpsIcon} alt="Localização" onClick={() => handleGeolocationSearch(lastLat, lastLon)} />
           <img src={homeIcon} alt="Voltar" onClick={onBack} />
           <img src={!infosView?infoIcon:closeIcon} alt="Info" onClick={handleInfosView} />
         </div>
